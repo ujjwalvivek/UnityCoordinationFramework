03f00d09745b12b394f0644e78cb1c3fbff5e688
Ujjwal Kumar <ujjwalvivek21@gmail.com>
2021-10-31 01:36:20 +0530

    Bug Fixes For Task A

--- Lila Games/Assets/Scripts/Task A/RemotePlayerMovement.cs
+++ Lila Games/Assets/Scripts/Task A/RemotePlayerMovement.cs
@@ -33,12 +33,14 @@ public class RemotePlayerMovement : MonoBehaviour
     void ProcessMovement()
     {
         Vector2 tempPos = localPlayer.LocalPlayerPosition();
+        Debug.Log("Co-ordinates Received (X and Z): " + new Vector2(tempPos.x, tempPos.y) + " with a size of " + (sizeof(float) * 2) + " Bytes");
         remoteRB.MovePosition(new Vector3(tempPos.x, transform.position.y, tempPos.y + 20f)); 
     }
 
     void ProcessRotation()
     {
         Vector3 tempRot = new Vector3(transform.rotation.x, localPlayer.LocalPlayerRotation(), transform.rotation.eulerAngles.z);
+        Debug.Log("Angles Received (Y): " + tempRot.y + " with a size of " + sizeof(float) + " Bytes");
         remoteRB.MoveRotation(Quaternion.Euler(tempRot));
     }
 }
